b6e05577990badd4fdaaedc9738e5d92d7e3d38e
ZhiFeng Hu <hufeng1987@gmail.com>
2015-08-30 07:31:32 +0800

    Remove unused UI
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- app/src/main/java/com/netroby/app/viewtxt/MainActivity.java
+++ app/src/main/java/com/netroby/app/viewtxt/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mainContent = (TextView) findViewById(R.id.mainContent);
+        mainContent.setText("This is not a test, it will loading data from network");
     }
 
     @Override
